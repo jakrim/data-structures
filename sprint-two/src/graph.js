aceccd50347c5ecb5f98a10da2f48a7045c4a34e
“Jesse <jesse.krim@gmail.com>
2019-05-31 16:54:31 -0400

    Working on src/graph - Completed addNode, contains, hasEdge.

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -4,6 +4,7 @@ storage = {
   2: [5. 3],
   3: [2]
 }
+
 storage = {
   5: {2, true},
   2: {5, true, 3: true}
@@ -14,45 +15,64 @@ storage = {
 
 // Instantiate a new graph
 var Graph = function() {
-  this._storage = {};
+  this.storage = {};
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
-  this._storage[node] = true;
-  // console.log('this._storage', this._storage)
+  
+    this.storage[node] = [];
+  
+  console.log('storage', this.storage);
+  // console.log('this.storage', this.storage)
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
-  for (var key in this._storage) {
-    if (node === key) {
-      return true;
-    }
+  if (this.storage.hasOwnProperty(node)) {
+    return true;
   }
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
-  delete this._storage[node];
-};
-
-// Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
-Graph.prototype.hasEdge = function(fromNode, toNode) {
-  this._storage[fromNode] - this._storage[toNode] ? true : false;
-
+  // var temp = this.storage[node];
+  // if (this.storage.hasOwnProperty(node)) {
+    //   delete this.storage[node];
+    // }
+    // return temp;
+  };
+  
+  // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
+  Graph.prototype.hasEdge = function(fromNode, toNode) {
+    // this.storage[fromNode] - this.storage[toNode] ? true : false;
+    if (this.storage[fromNode].includes(toNode) && this.storage[toNode].includes(fromNode)) {
+      return true;
+    }
+    return false;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this.storage[fromNode].push(toNode);
+  this.storage[toNode].push(fromNode);
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  if (this.storage[fromNode].includes[toNode]) {
+    delete this.storage.fromNode[toNode];
+    delete this.storage.toNode[fromNode];
+  }
+  if (this.storage[toNode].includes[fromNode]) {
+    delete this.storage.fromNode[toNode];
+    delete this.storage.toNode[fromNode];
+  }
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+
 };
 
 /*
